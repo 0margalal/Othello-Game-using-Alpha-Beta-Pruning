5398fc5fa8987c58ceacd4a5abb829c0dfff40d7
Omar Galal Hassan Marghany <99077574+0margalal@users.noreply.github.com>
2024-05-10 22:30:33 +0300

    Update main.py

--- main.py
+++ main.py
@@ -11,11 +11,9 @@ def main():
     playerType = input('Choose White player type (human/AI): ')
     players.append(Player(playerType, 'W'))
 
-    depth = int(input('Choose difficulty 1-6:'))
-    while depth < 1:
-        depth = int(input('Difficulty must be greater than 0. Choose difficulty 1-6: '))
-    while depth > 6:
-        depth = int(input('Difficulty must be less than 7. Choose difficulty 1-6: '))
+    depth = int(input('Choose difficulty 1: Easy - 3: Medium - 5: Hard'))
+    while depth not in [1,3,5]:
+        depth = int(input('Difficulty must be either 1: Easy or 3: Medium or 5: Hard '))
 
     print('Game Start')
 
@@ -25,16 +23,17 @@ def main():
         board.displayBoard()
         player = players[count % 2]
         possible_moves = board.getPossibleMoves(player)
-        print('Possible moves: ', possible_moves)
         if len(possible_moves) == 0:
             print('No possible moves. Turn skipped.')
             count += 1
             continue
+        print('Possible moves: ', possible_moves)
         if player.player_type == 'human':
             move = input('Enter move: ').split()
             move = (int(move[0]), int(move[1]))
             while move not in possible_moves:
                 move = input('Invalid move. Enter move: ').split()
+                print('Possible moves: ', possible_moves)
                 move = (int(move[0]), int(move[1]))
             board.makeMove(player, move)
         else:
